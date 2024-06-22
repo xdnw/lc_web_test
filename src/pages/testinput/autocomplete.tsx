030aea7ad82d93b1da52a6aeeb5b0b566a9815f8
test <jessepaleg@outlook.com>
2024-06-22 18:41:42 +1000

    autocomplete

--- src/pages/testinput/autocomplete.tsx
+++ src/pages/testinput/autocomplete.tsx
@@ -1,9 +1,20 @@
+import { getCommands } from '@/utils/CommandUtil';
+import { withCommands } from '@/utils/StateUtil';
+import { useEffect, useState } from 'react';
 import TextInput from 'react-autocomplete-input';
 import 'react-autocomplete-input/dist/bundle.css';
 
 
 export default function AutoComplete() {
+    const type = "DBNation";
+    let [options, setOptions] = useState<string[]>([]);
+
     return <>
-    <TextInput className="bg-background" options={["apple", "apricot", "banana", "carrot"]} trigger="#" />
+    <TextInput matchAny={true} className="bg-background" requestOnlyIfNoOptions={true} options={options} trigger="#" maxOptions={8} onRequestOptions={() => {
+        withCommands().then(async f => {
+            const obj = f.data.placeholders[type];
+            setOptions(Object.keys(obj));
+        });
+    }}/>
     </>
 }
